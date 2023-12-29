956d4d017b9a2404950d2831c2810229c648bf6d
Lumenohr <149939320+Lumenohr@users.noreply.github.com>
2023-12-29 23:15:18 +0800

    Update pinejinx.sh

--- pinejinx.sh
+++ pinejinx.sh
@@ -62,9 +62,9 @@ install() {
 		printf "Extraction failed!\nAborting...\n"
 		exit
 	fi
-	curl -s -L "https://raw.githubusercontent.com/al/Pine-jinx/main/Ryujinx.desktop" > Ryujinx.desktop
-	curl -s -L "https://raw.githubusercontent.com/al/Pine-jinx/main/Ryujinx.png" > Ryujinx.png
-	curl -s -L "https://raw.githubusercontent.com/al/Pine-jinx/main/Ryujinx.xml" > Ryujinx.xml
+	curl -s -L "https://raw.githubusercontent.com/Lumenohr/Pine-jinx/main/Ryujinx.desktop" > Ryujinx.desktop
+	curl -s -L "https://raw.githubusercontent.com/Lumenohr/Pine-jinx/main/Ryujinx.png" > Ryujinx.png
+	curl -s -L "https://raw.githubusercontent.com/Lumenohr/Pine-jinx/main/Ryujinx.xml" > Ryujinx.xml
 	if [ "$noconfirm" = "1" ]; then
 		:
 	else
@@ -72,7 +72,7 @@ install() {
 	fi
 	if [ "$gamemode" = "y" ] || [ "$gamemode" = "Y" ]; then
 		arg1="gamemoderun "
-		curl -s -L "https://raw.githubusercontent.com/al/Pine-jinx/main/gamemode.ini" > /home/${USER}/.config/gamemode.ini
+		curl -s -L "https://raw.githubusercontent.com/Lumenohr/Pine-jinx/main/gamemode.ini" > /home/${USER}/.config/gamemode.ini
 	else
 		arg1=""
 	fi
